65abdfc1a977312934580533847b9ce99f7ab893
Francesco Burelli <francesco.burelli@gmail.com>
2020-03-19 01:01:56 +0100

    JuicyPixels library added

--- src/ImportMedia.hs
+++ src/ImportMedia.hs
@@ -3,12 +3,16 @@
 
 module ImportMedia ( postApiMedia ) where
 
+import qualified Codec.Picture               as P
+import qualified Codec.Picture.Metadata      as M
+import qualified Codec.Picture.Metadata.Exif as E
+
 import           Control.Monad.IO.Class
 
-import           Data.Aeson             ( ToJSON )
+import           Data.Aeson                  ( ToJSON )
 import           Data.Text
 
-import qualified Folders                as F
+import qualified Folders                     as F
 
 import           GHC.Generics
 
@@ -33,9 +37,18 @@ importMedia homePath = mapM (importSingleMedia homePath)
 
 importSingleMedia :: Text -> String -> IO Result
 importSingleMedia homePath mediaToImport = do
-    let filePath = (unpack homePath) ++ (F.surroundWithSlashes F.toImport)
-            ++ mediaToImport
+    result <- P.readImageWithMetadata filePath
+    case result of
+        Left message -> return Result { toImport = mediaToImport
+                                      , result   = message
+                                      , media    = Nothing
+                                      }
+        Right (_, metadatas) -> importMediaWithMetadatas filePath metadatas
+  where
+    filePath =
+        unpack homePath ++ F.surroundWithSlashes F.toImport ++ mediaToImport
+
+importMediaWithMetadatas :: FilePath -> M.Metadatas -> IO Result
+importMediaWithMetadatas filePath metadatas = do
+    let dateTimeOriginal = M.lookup (M.Exif $ E.TagUnknown 0x9003) metadatas
     return Result {  }
-
-
-
