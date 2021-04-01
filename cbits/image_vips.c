9614d16b0b90d2e34b90baed1e5ecf835ef4398e
Francesco Burelli <francesco.burelli@gmail.com>
2021-04-01 23:59:29 +0200

    exif_string implemented

--- cbits/image_vips.c
+++ cbits/image_vips.c
@@ -37,14 +37,11 @@ typedef struct Metadata {
   char * gpsAltitudeRef;
 } Metadata;
 
-int exif(char *filename) {
-  VipsImage *image = vips_image_new_from_file(filename, NULL);
+int exif_string(VipsImage *image, const char *name, char ** out) {
   GValue value = { 0 };
-  char *name = "exif-ifd2-DateTimeOriginal";
   int result = vips_image_get (image, name, &value);
   if (result != 0) {
     printf("vips_image_get error %d", result);
-    g_object_unref(image);
     return result;
   }
   if (G_VALUE_TYPE (&value) != G_TYPE_STRING) {
@@ -53,9 +50,24 @@ int exif(char *filename) {
   }
   size_t sValueLength = 0;
   const char * sValue = vips_value_get_ref_string(&value, &sValueLength);
-  g_object_unref(image);
+  *out = malloc(sValueLength + 1);
+  memcpy(*out, sValue, sValueLength + 1);
   printf("%s = %s (%zu)\n", name, sValue, sValueLength);
   g_value_unset (&value);
+  return 0;
+}
+int exif(char *filename) {
+  VipsImage *image = vips_image_new_from_file(filename, NULL);
+  const char *name = "exif-ifd2-DateTimeOriginal";
+  char * sValue = NULL;
+  int result = exif_string(image, name, &sValue);
+  if (result != 0) {
+    printf("exif_string error %d\n", result);
+    g_object_unref(image);
+    return result;
+  }
+  printf("%s = %s\n", name, sValue);
+  g_object_unref(image);
   return 0;
 }
 int main(int argc, char ** argv) {
