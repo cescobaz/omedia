d74b1d2f6ab76533ce2b8a86446e87080cce569c
Francesco Burelli <francesco.burelli@gmail.com>
2021-04-03 21:26:56 +0200

    README improved with requirements; removed useless function

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # omedia
 organize your media
 
+## Requirements
+
+* [libejdb2](https://github.com/Softmotions/ejdb#use-cases)
+* [libvips](https://github.com/libvips/libvips)
+
 ## Build and Run
 ```bash
 cabal v2-run
--- src/MediaThumbnails.hs
+++ src/MediaThumbnails.hs
@@ -86,15 +86,3 @@ createThumbnail directory (width, height) filePath =
                      , width    = thumbnailWidth
                      , height   = thumbnailHeight
                      }
-
-scaleSize :: (Int, Int) -> (Int, Int) -> (Int, Int)
-scaleSize (maxWidth, maxHeight) (originalWidth, originalHeight)
-    | (originalWidth <= maxWidth) && (originalHeight <= maxHeight) =
-        (originalWidth, originalHeight)
-    | widthRatio < heightRatio =
-        (maxWidth, round $ widthRatio * fromIntegral originalHeight)
-    | otherwise = (round $ fromIntegral originalWidth * heightRatio, maxHeight)
-  where
-    widthRatio = fromIntegral maxWidth / fromIntegral originalWidth :: Double
-
-    heightRatio = fromIntegral maxHeight / fromIntegral originalHeight :: Double
